0a1ac97f6f17c458cfc4d2dd17803140a8923561
UzakoffUmar <uzakovumar338@gmail.com>
2023-08-03 22:37:15 +0500

    menu added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Layout } from "./components";
-import { HomePage, LoginPage, RegisterPage, VerificationPage } from "./pages";
+import { HomePage, LoginPage, RegisterPage, VerificationPage, MenuPage } from "./pages";
 import { Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,6 +13,7 @@ const App = () => {
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/register" element={<RegisterPage />} />
           <Route path="/auth/register/verification" element={<VerificationPage />} />
+          <Route path="/menu" element={<MenuPage />} />
         </Routes>
       </Layout>
       <ToastContainer />
